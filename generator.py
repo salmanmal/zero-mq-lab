6c09039dc8935022d94db3b0f779366ae3aeb394
Salman Mal <salmanmal@yahoo.com>
2020-09-29 23:58:00 -0700

    Multiple worker monitoring same queue and sharing load

--- generator.py
+++ generator.py
@@ -1,15 +1,19 @@
 import zmq
 import sys
+import time
 
 # ZeroMQ Context
 context=zmq.Context()
 
 # Define the socket using the "Context"
+# Bind is server
 sock=context.socket(zmq.PUSH)
-sock.connect("tcp://127.0.0.1:3001")
+sock.bind("tcp://127.0.0.1:3001")
 
 # Send a "message" using the socket
-for i in range(10001):
+for i in range(10):
+    time.sleep(1)
+    print(i)
     sock.send("{}".format(i).encode())
 
-# print(sock.recv().decode())
+sock.close()
--- worker.py
+++ worker.py
@@ -1,24 +1,34 @@
 import zmq
 import math
+import time
 
-
-# ZeroMQ Context
-context =zmq.Context()
-
-
-# Define the socket using the "Context"
-sock=context.socket(zmq.PULL)
-sock.bind("tcp://127.0.0.1:3001")
-
-
-context2 =zmq.Context()
-dashboard=context2.socket(zmq.PUSH)
-dashboard.connect("tcp://127.0.0.1:3000")
 
 # Run a simple "Echo" Server
 while True:
-    message=sock.recv()
-    message=message.decode()
-    reply_msg="Square root of {} = {}".format(message,math.sqrt(int(message)))
-    dashboard.send(reply_msg.encode())
-    print(reply_msg)
+    # ZeroMQ Context
+    try :
+        context =zmq.Context()
+        # Define the socket using the "Context"
+        sock=context.socket(zmq.PULL)
+        sock.connect("tcp://127.0.0.1:3001")
+        message=sock.recv()
+        sock.close()
+        message=message.decode()
+        if message:
+            context2 =zmq.Context()
+            dashboard=context2.socket(zmq.PUSH)
+            dashboard.bind("tcp://127.0.0.1:3000")
+            reply_msg="Square root of {} = {}".format(message,math.sqrt(int(message)))
+            print(reply_msg)
+            dashboard.send(reply_msg.encode())
+            dashboard.close()
+    except:
+        print("connection busy retry")
+    time.sleep(3)
+
+
+    
+    
+    
+    
+    
